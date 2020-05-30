2e1e12ac478d255d0688ba9ec16e363036f8375e
Attednev <damian.gasser@gmail.com>
2020-05-30 12:51:55 +0200

    Added controller functions and added field class

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -1,4 +1,30 @@
 package Main;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
 public class Controller {
+    @FXML private GridPane gameRoot;
+    @FXML private VBox menuRoot;
+    private boolean xTurn = true;
+
+    @FXML private void initialize() {
+        this.gameRoot.setVisible(false);
+    }
+
+    @FXML private void startSinglePlayer() {
+        this.changeSceneToGame();
+    }
+
+    @FXML private void startMultiPlayer() {
+        this.changeSceneToGame();
+    }
+
+    private void changeSceneToGame() {
+        this.gameRoot.setVisible(true);
+        this.menuRoot.setVisible(false);
+    }
+
 }
---
+++ src/TicTacToe/Field.java
@@ -0,0 +1,11 @@
+package TicTacToe;
+
+import javafx.scene.shape.Rectangle;
+
+public class Field extends Rectangle {
+
+    public Field() {
+
+    }
+
+}
