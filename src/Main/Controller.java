46dbada87f0c050331d2aebebdbbc1d14d5af966
Attednev <damian.gasser@gmail.com>
2020-05-30 14:51:31 +0200

    Added win conditions

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -1,6 +1,7 @@
 package Main;
 
 import TicTacToe.Field;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
@@ -12,6 +13,7 @@ public class Controller {
     @FXML private GridPane gameRoot;
     @FXML private VBox menuRoot;
     private boolean xTurn = true;
+    private boolean singlePlayer = true;
 
     @FXML private void initialize() {
         this.root.setStyle("-fx-background-color: rgb(48, 50, 48)");
@@ -25,6 +27,7 @@ public class Controller {
     }
 
     @FXML private void startMultiPlayer() {
+        this.singlePlayer = false;
         this.changeSceneToGame();
     }
 
@@ -38,8 +41,38 @@ public class Controller {
         int state = field.getState();
         if (state == 0) {
             field.setState(this.xTurn ? 1 : -1);
-            this.xTurn = !this.xTurn;
+            if (this.checkEndGame()) {
+                System.out.println((this.xTurn ? "X" : "O" )+ "won");
+                this.endGame();
+            }
+            if (this.singlePlayer) {
+                this.aiMove();
+            } else {
+                this.xTurn = !this.xTurn;
+            }
         }
     }
+
+    private void aiMove() {
+
+    }
+
+    private boolean checkEndGame() {
+        ObservableList<Node> nodes = this.gameRoot.getChildren();
+        return (((Field)nodes.get(0)).getState() == ((Field)nodes.get(1)).getState() && ((Field)nodes.get(0)).getState() == ((Field)nodes.get(2)).getState() && ((Field)nodes.get(0)).getState() != 0 ||
+                ((Field)nodes.get(3)).getState() == ((Field)nodes.get(4)).getState() && ((Field)nodes.get(3)).getState() == ((Field)nodes.get(5)).getState() && ((Field)nodes.get(3)).getState() != 0 ||
+                ((Field)nodes.get(6)).getState() == ((Field)nodes.get(7)).getState() && ((Field)nodes.get(6)).getState() == ((Field)nodes.get(8)).getState() && ((Field)nodes.get(6)).getState() != 0 ||
+                ((Field)nodes.get(6)).getState() == ((Field)nodes.get(7)).getState() && ((Field)nodes.get(6)).getState() == ((Field)nodes.get(8)).getState() && ((Field)nodes.get(6)).getState() != 0 ||
+                ((Field)nodes.get(0)).getState() == ((Field)nodes.get(3)).getState() && ((Field)nodes.get(0)).getState() == ((Field)nodes.get(6)).getState() && ((Field)nodes.get(0)).getState() != 0 ||
+                ((Field)nodes.get(1)).getState() == ((Field)nodes.get(4)).getState() && ((Field)nodes.get(1)).getState() == ((Field)nodes.get(7)).getState() && ((Field)nodes.get(1)).getState() != 0 ||
+                ((Field)nodes.get(2)).getState() == ((Field)nodes.get(5)).getState() && ((Field)nodes.get(2)).getState() == ((Field)nodes.get(8)).getState() && ((Field)nodes.get(2)).getState() != 0 ||
+                ((Field)nodes.get(0)).getState() == ((Field)nodes.get(4)).getState() && ((Field)nodes.get(0)).getState() == ((Field)nodes.get(8)).getState() && ((Field)nodes.get(0)).getState() != 0 ||
+                ((Field)nodes.get(2)).getState() == ((Field)nodes.get(4)).getState() && ((Field)nodes.get(2)).getState() == ((Field)nodes.get(6)).getState() && ((Field)nodes.get(2)).getState() != 0);
+    }
+
+    private void endGame() {
+
+    }
+
 
 }
