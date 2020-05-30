94c6ba45876460d23f33ef3938351bfa2b06b6a9
Attednev <damian.gasser@gmail.com>
2020-05-30 14:16:09 +0200

    Added grid creation

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -1,17 +1,23 @@
 package Main;
 
+import TicTacToe.Field;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 public class Controller {
+    @FXML private BorderPane root;
     @FXML private GridPane gameRoot;
     @FXML private VBox menuRoot;
     private boolean xTurn = true;
 
     @FXML private void initialize() {
+        this.root.setStyle("-fx-background-color: rgb(48, 50, 48)");
         this.gameRoot.setVisible(false);
+        this.gameRoot.setStyle("-fx-border-color: rgb(48, 50, 48)");
+        for (int i = 0; i < 3; i++) this.gameRoot.addColumn(i, new Field(i * 3 + 1), new Field(i * 3 + 2), new Field(i * 3 + 3));
     }
 
     @FXML private void startSinglePlayer() {
--- src/TicTacToe/Field.java
+++ src/TicTacToe/Field.java
@@ -1,11 +1,26 @@
 package TicTacToe;
 
+import Main.Controller;
+import javafx.geometry.Insets;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
-public class Field extends Rectangle {
+public class Field extends StackPane {
 
-    public Field() {
-
+    public Field(int number, Controller controller) {
+        super();
+        this.setPrefSize(100, 100);
+        this.setStyle("-fx-border-color: " +
+                (number != 1 && number != 4 && number != 7 ? "orange" : "#303230") + " " +
+                (number < 7 ? "orange" : "#303230") + " " +
+                (number % 3 != 0 ? "orange" : "#303230") + " " +
+                (number > 3 ? "orange" : "#303230") +
+                "; -fx-background-color: #303230");
+        this.setBackground(new Background(new BackgroundFill(Color.rgb(48, 50, 48), CornerRadii.EMPTY, Insets.EMPTY)));
+        this.setOnMouseEntered(e -> this.setBackground(new Background(new BackgroundFill(Color.rgb(82, 85, 82), CornerRadii.EMPTY, Insets.EMPTY))));
+        this.setOnMouseExited(e -> this.setBackground(new Background(new BackgroundFill(Color.rgb(48, 50, 48), CornerRadii.EMPTY, Insets.EMPTY))));
     }
 
 }
